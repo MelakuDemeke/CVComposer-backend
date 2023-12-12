50e6c58678b52109c237c58badb4f670d8b5a64e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-12 16:10:23 +0300

    Add create job functionality

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,10 +1,17 @@
 import express from "express";
+import Job from "../models/Job.js";
 
 const router = express.Router();
 
 // TODO: CREATE JOB
-router.post("/", (req, res) => {
-    
+router.post("/", async (req, res) => {
+    const newJob = new Job(req.body);
+    try{
+        const savedJob = await newJob.save();
+        res.status(200).json(savedJob);
+    }catch(error){
+        res.status(500).json(error);
+    }
 });
 // TODO: UPDATE JOB
 // TODO: DELETE JOB
