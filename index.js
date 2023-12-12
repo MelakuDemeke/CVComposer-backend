be3cd6d1915d22270c309f2b4c0d8449d606f07c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-12 14:41:35 +0300

    connect to mongo

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Database connected");
+    } catch (error) {
+        throw error;
+    }
+};
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
 app.listen(8800, () => {
+    connect();
     console.log("Server is running");
-}
-);
+});
