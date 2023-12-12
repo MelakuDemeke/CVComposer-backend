f4539f09fc273e8762fecd725cb6ec86fe145401
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-12 22:43:08 +0300

    Refactor job routes and controllers

---
+++ controllers/jobs.js
@@ -0,0 +1,55 @@
+import Job from "../models/Job.js";
+
+export const createJob = async (req, res, next) => {
+    const newJob = new Job(req.body);
+    try {
+        const savedJob = await newJob.save();
+        res.status(200).json(savedJob);
+    } catch (error) {
+        next(error)
+    }
+};
+
+export const updateJob = async (req, res, next) => {
+    try {
+        const updatedJob = await Job.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        );
+        res.status(200).json(updatedJob);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+export const deleteJob = async (req, res, next) => {
+    try {
+        await Job.findByIdAndDelete(
+            req.params.id,
+        );
+        res.status(200).json("Job has been deleted");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+export const getJob = async (req, res, next) => {
+    try {
+        const job = await Job.findById(
+            req.params.id,
+        );
+        res.status(200).json(job);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+export const getJobs = async (req, res, next) => {
+    try {
+        const jobs = await Job.find();
+        res.status(200).json(jobs);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,61 +1,19 @@
 import express from "express";
 import Job from "../models/Job.js";
+import { createJob, deleteJob, getJob, getJobs, updateJob } from "../controllers/jobs.js";
 
 const router = express.Router();
 
-// TODO: CREATE JOB
-router.post("/", async (req, res) => {
-    const newJob = new Job(req.body);
-    try {
-        const savedJob = await newJob.save();
-        res.status(200).json(savedJob);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
-// TODO: UPDATE JOB
-router.put("/:id", async (req, res) => {
-    try {
-        const updatedJob = await Job.findByIdAndUpdate(
-            req.params.id,
-            { $set: req.body },
-            { new: true }
-        );
-        res.status(200).json(updatedJob);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
-// TODO: DELETE JOB
-router.delete("/:id", async (req, res) => {
-    try {
-        await Job.findByIdAndDelete(
-            req.params.id,
-        );
-        res.status(200).json("Job has been deleted");
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
-// TODO: GET JOB
-router.get("/:id", async (req, res) => {
-    try {
-        const job = await Job.findById(
-            req.params.id,
-        );
-        res.status(200).json(job);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
-// TODO: GETALL JOB
-router.get("/", async (req, res) => {
-    try {
-        const jobs = await Job.find();
-        res.status(200).json(jobs);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
+//CREATE JOB
+router.post("/", createJob);
+
+//UPDATE JOB
+router.put("/:id", updateJob);
+//DELETE JOB
+router.delete("/:id",deleteJob);
+//GET JOB
+router.get("/:id", getJob);
+//GETALL JOB
+router.get("/", getJobs);
 
 export default router;
