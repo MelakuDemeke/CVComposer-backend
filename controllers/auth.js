8a656eb7b3fff8ec42e4a1ccbe8dac14dcc349a8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-12 23:23:04 +0300

    Add user registration functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
+import User from "../models/User.js";
+
 export const register = async (req, res, next) => {
     try {
-
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        await newUser.save();
+        res.status(200).send("user has been created");
     } catch (error) {
         next(error)
     }
