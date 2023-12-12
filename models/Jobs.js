412b647cce77261a8bcd1232cd7d491ae161ffc6
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-12 15:19:44 +0300

    Add jobSchema to Jobs.js

--- models/Jobs.js
+++ models/Jobs.js
@@ -1,2 +1,25 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
+
+const jobSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    company: {
+        type: String,
+        required: true,
+    },
+    location: String,
+    description: String,
+    salary: {
+        type: Number,
+        min: 0,
+    },
+    postedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    qualifications: [String],
+    applicationDeadline: Date,
+});
