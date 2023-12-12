eb6c45594f9d36217e7fc3612c17b5a87c20264e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-12 15:12:56 +0300

    Add route to create job

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,6 +3,9 @@ import express from "express";
 const router = express.Router();
 
 // TODO: CREATE JOB
+router.post("/", (req, res) => {
+    
+});
 // TODO: UPDATE JOB
 // TODO: DELETE JOB
 // TODO: GET JOB
