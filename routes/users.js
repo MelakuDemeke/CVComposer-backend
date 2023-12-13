11285d3f8baeb5039a6b6ff14247ade874a24741
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-13 12:04:30 +0300

    Add authentication check route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import { updateUser, deleteUser, getUser, getUsers } from '../controllers/users.js';
+import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next)=>{
+    res.send("You are authenticated");
+});
 
 //UPDATE User
 router.put("/:id", updateUser);
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,4 +1,4 @@
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import {createError} from "../utils/error.js"
 
 export const verifyToken = (req, res, next) => {
