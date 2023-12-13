bd4c12abcf1160f20a02cacd21727adfd72a6553
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-13 10:32:27 +0300

    Update password encryption in register function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,15 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res, next) => {
     try {
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hash
         })
         await newUser.save();
         res.status(200).send("user has been created");
@@ -13,3 +17,6 @@ export const register = async (req, res, next) => {
         next(error)
     }
 };
+
+
+
