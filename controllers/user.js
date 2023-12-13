5915c519454430613aa186f41c999254aba8a39d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-13 11:23:03 +0300

    add user controller

---
+++ controllers/user.js
@@ -0,0 +1,45 @@
+import User from "../models/User.js";
+
+export const updateUser = async (req, res, next) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        );
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        next(error)
+    }
+};
+
+export const deleteUser = async (req, res, next) => {
+    try {
+        await User.findByIdAndDelete(
+            req.params.id,
+        );
+        res.status(200).json("User has been deleted");
+    } catch (error) {
+        next(error)
+    }
+};
+
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(
+            req.params.id,
+        );
+        res.status(200).json(user);
+    } catch (error) {
+        next(error)
+    }
+};
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        next(error)
+    }
+};
