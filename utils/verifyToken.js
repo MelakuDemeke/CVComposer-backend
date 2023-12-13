c25dee834b382d4937efa9d0bf196e0a9dfda73e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-12-13 11:54:20 +0300

    Add token verification using JWT

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -6,5 +6,9 @@ export const verifyToken = (req, res, next) => {
 
     if(!token) return next(createError(401, "You are not authenticated!"));
 
-    
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if(err) return next(createError(403, "Token is not valid!"));
+        req.user = user;
+        next();
+    });
 }
